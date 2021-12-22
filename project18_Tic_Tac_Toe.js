60210dd2026e3e117c46157866e4a7b06af1a24e
Sanjay soni <96236466+SanjaySonii@users.noreply.github.com>
2021-12-22 12:17:18 +0530

    Update project18_Tic_Tac_Toe.js

--- project18_Tic_Tac_Toe.js
+++ project18_Tic_Tac_Toe.js
@@ -7,13 +7,13 @@ const item5 = ['2', '4', '6'];
 const item6 = ['2', '5', '8'];
 const item7 = ['3', '4', '5'];
 const item8 = ['6', '7', '8'];
-const win = [item1,item2,item3,item4,item5,item6,item7,item8];
+const win = [item1, item2, item3, item4, item5, item6, item7, item8];
 const O = [];
 const X = [];
 
 let refresh = document.getElementById(`refresh`);
-refresh.addEventListener(`click`,(e)=>{
-window.location.reload();
+refresh.addEventListener(`click`, (e) => {
+    window.location.reload();
 })
 
 var Turn = document.getElementById(`Turn`);
@@ -24,13 +24,13 @@ var meriChoice;
 for (const radio of myChoice) {
     radio.addEventListener('click', (e) => {
         if (radio.value == 'circle') {
-            choosed.innerText = "You chose Circle";
+            choosed.innerText = "Player 1 : Circle , Player 2 : Cross";
             meriChoice = "Circle";
             C = 'Circle';
             Turn.innerText = "Circle's Turn";
         }
         else {
-            choosed.innerText = "You chose Cross";
+            choosed.innerText = "Player 1 : Cross , Player 2 : Circle";
             meriChoice = "Cross";
             C = 'Cross';
             Turn.innerText = "Cross's Turn";
@@ -60,7 +60,7 @@ for (const bttn of btn) {
                 compare();
                 Turn.innerText = "Cross's Turn";
             }
-            else if(C == 'Cross'){
+            else if (C == 'Cross') {
                 bttn.innerHTML = `<img class="O" src="X1.jpg">`;
                 X.push(index);
                 console.log(X);
@@ -68,7 +68,7 @@ for (const bttn of btn) {
                 compare();
                 Turn.innerText = "Circle's Turn";
             }
-            else{
+            else {
                 alert("Please choose an option");
             }
 
@@ -81,58 +81,42 @@ for (const bttn of btn) {
 
 function compare() {
     for (const item of win) {
-        // console.log(item);
         if (O.length >= 3) {
             let count = 0;
             for (let i = 0; i < 3; i++) {
                 if (O.includes(item[i])) {
                     count++;
+                    if (count == 3) {
+                        console.log('O wins');
+                        choosed.innerText = "Congratulations Circle You Won";
+                        choosed.classList.add(`animation1`);
+                        Turn.classList.add("display");
+                        onOf = "Of";
+                    }
                 }
-            }
-            // console.log(count);
-            if(count == 3){
-                console.log('O wins');
-                if(meriChoice == "Cross"){
-                    choosed.innerText = "Oops! You Lose";
-                    choosed.classList.add(`animation1`);
-                    Turn.classList.add("display");
-                    // Turn.innerText = "You Lose";
-                }
-                else{
-                    choosed.innerText = "Congratulations You Won";
-                    choosed.classList.add(`animation1`);
-                    Turn.classList.add("display");
-                    // Turn.innerText = "You Won";
-                }
-                onOf = "Of";
             }
         }
-        if (X.length >= 3) {
+         if (X.length >= 3) {
             let count = 0;
             for (let i = 0; i < 3; i++) {
                 if (X.includes(item[i])) {
                     count++;
+                    if (count == 3) {
+                        console.log('X wins');
+                        choosed.innerText = "Congratulations Cross You Won";
+                        choosed.classList.add(`animation1`);
+                        Turn.classList.add("display");
+                        onOf = "Of";
+                    }
                 }
-            }
-            // console.log(count);
-            if(count == 3){
-                console.log('X wins');
-                if(meriChoice == "Cross"){
-                    choosed.innerText = "Congratulations You Won";
-                    choosed.classList.add(`animation1`);
-                    // Turn.innerText = "You Won";
-                    Turn.classList.add("display");
-                }
-                else{
-                    choosed.innerText = "Oops! You Lose";
-                    choosed.classList.add(`animation1`);
-                    // Turn.innerText = "You Lose";
-                    Turn.classList.add("display");
-                }
-                onOf = "Of";
             }
         }
-        
+        if((X.length == 4 && O.length == 5 || O.length == 4 && X.length == 5) && onOf == "On"){
+            console.log("nobody won");
+            choosed.innerText = "Oops! nobody Won , Click on the refresh button to play again";
+            Turn.classList.add("display");
+        }
     }
 
 }
+
